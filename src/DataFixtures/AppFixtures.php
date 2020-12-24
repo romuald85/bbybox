cdc4985905de049c69562f9634ec721722a6e1fe
romuald85 <papeboyedk@gmail.com>
2020-12-24 22:43:27 +0100

    Create Category entity, feature DataFixtures and make template error

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\Product;
+use Liior\Faker\Prices;
+use Bezhanov\Faker\Provider\Commerce;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class AppFixtures extends Fixture
+{
+    private $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+        $faker->addProvider(new \Liior\Faker\Prices($faker));
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
+
+        for ($p = 0; $p < 100; $p++) {
+            $product = new Product;
+            $product->setName($faker->productName())
+                ->setPrice($faker->price(1000, 5000))
+                ->setSlug(strtolower($this->slugger->slug($product->getName())));
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+    }
+}
